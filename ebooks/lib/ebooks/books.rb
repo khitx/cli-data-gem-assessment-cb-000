7e0f5f24cdad2844de8012425d3d5d0bd15f7dc2
Kathy Hudson <khitx@icloud.com>
2018-04-26 16:39:46 +0000

    automatically backed up by learn

--- ebooks/lib/ebooks/books.rb
+++ ebooks/lib/ebooks/books.rb
@@ -7,15 +7,6 @@ class Ebooks::Books
     books
   end
 
-  def self.scrape_BookBubxxx
-    doc = Nokogiri::HTML(open("https://www.bookbub.com/ebook-deals/free-ebooks"))
-    book = self.new
-    book.title = doc.search("div.book-title a").first.text.strip
-    book.author = doc.search("small.book-author a").first.text.strip
-    book.desc = doc.search("p.blurb").first.text.strip
-    book
-  end
-
   def self.scrape_BookBub
     doc = Nokogiri::HTML(open("https://www.bookbub.com/ebook-deals/free-ebooks"))
     books = []
