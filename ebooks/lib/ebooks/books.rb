380c5091e2c9aa26becaf15980c62e7bc158aabc
Kathy Hudson <khitx@icloud.com>
2018-04-26 15:23:35 +0000

    automatically backed up by learn

--- ebooks/lib/ebooks/books.rb
+++ ebooks/lib/ebooks/books.rb
@@ -19,8 +19,9 @@ class Ebooks::Books
   def self.scrape_BookBub
     doc = Nokogiri::HTML(open("https://www.bookbub.com/ebook-deals/free-ebooks"))
     books = []
-    binding.pry
+#    binding.pry
     doc.css("div.book-panel").each do |book|
+      binding.pry
       book = self.new
       book.title = doc.search("div.book-title a").text.strip
       book.author = doc.search("small.book-author a").text.strip
